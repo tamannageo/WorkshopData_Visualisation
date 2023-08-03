52bed53e183a11ecd6a8eabf01c49f5befbb7c1c
tamannageo <141242255+tamannageo@users.noreply.github.com>
2023-08-03 12:18:12 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -11,7 +11,7 @@ for i in os.listdir():
 df =  pd.read_csv('Bastar Craton.csv')
 st.dataframe(df)
 #el_list = df.columns.tolist()[27:80]
-x_axis = st.selectbox('select element', el_list)
+#x_axis = st.selectbox('select element', el_list)
 
 
 st.multiselect('select location' , filename, filename[0])
