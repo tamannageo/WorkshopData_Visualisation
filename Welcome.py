314bdfed266882bb71923d48204bfbd30040a999
tamannageo <141242255+tamannageo@users.noreply.github.com>
2023-08-03 14:13:31 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -7,25 +7,26 @@ filename = []
 for i in os.listdir():
   if i.endswith('.csv'):
     df1 = pd.read_csv(i)
-    filename.append(i)
+    final_data=filename.append(i)
 
 st.write(filename) 
 df =  pd.read_csv('Bastar Craton.csv')
 st.dataframe(df)
 el_list = df.columns.tolist()[27:80]
 #x_axis = st.selectbox('select element', el_list)
-#st.multiselect('select location' , filename, filename[0])
+colours = ['blue', 'green', 'purple', 'pink', 'yellow', 'grey', 'black']
+st.multiselect('select location' , filename, filename[0])
 
-x = st.selectbox('x-axis', df.columns.tolist()[27:80])
-y = st.selectbox('y-axis', df.columns.tolist()[27:80], index =9)
+x = st.selectbox('x-axis', final_data.columns.tolist()[27:80])
+y = st.selectbox('y-axis', final_data.columns.tolist()[27:80], index =9)
 p = figure(
     title='Plot',
     x_axis_label= x + '(wt%)',
     y_axis_label= y + '(wt%)')
-mean1 = np.mean(df[x]/10000)
-mean2 = np.mean(df[y]/10000)
+mean1 = np.mean(final_data[x]/10000)
+mean2 = np.mean(final_data[y]/10000)
                 
-#p.circle(df[x]/10000, df[y]/10000, legend_label='Trend', line_width=2)
-p.line(mean1, mean2, line_width=2)
+p.circle(final_data[x]/10000, final_data[y]/10000, legend_label=filename, color= colours,  line_width=2)
+#p.line(mean1, mean2, line_width=2)
 st.bokeh_chart(p, use_container_width=True)
 st.write(mean1, mean2)
